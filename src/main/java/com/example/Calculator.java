920a0f4301b6597978c608c2e0960fc9fd85b19c
Harshitha1518 <devopslearning152018@gmail.com>
2026-01-29 01:23:07 +0530

    Refactor App class into Calculator with basic operations

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Calculator app started");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public boolean isPositive(int num) {
+        return num > 0;
+    }
+}
